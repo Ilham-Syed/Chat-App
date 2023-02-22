073f5190da1e710cae8e71404e95681421b60c71
Ilham-Syed <ilhamsyed1310@gmail.com>
2023-02-22 23:00:06 +0530

    Day1

---
+++ lib/shared/constant.dart
@@ -0,0 +1,6 @@
+class Constants{
+  static String appId="1:929833894333:web:e527b8896801f5b6c192d4";
+  static String apiKey="AIzaSyCUc1wxQx49xPpT-krDm-Z8zNRTCNDajtA";
+  static String messagingSenderId="929833894333";
+  static String projectId="chatappflutter-222c0";
+}
